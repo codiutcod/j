04704e93bd16d7d1db91d3da590d14e543ec86b0
codiutcod <90473203+codiutcod@users.noreply.github.com>
2021-11-08 20:20:17 +0700

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -154,7 +154,7 @@ keyboard = InlineKeyboardMarkup(
                 text="System Stats 💻",
                 callback_data="stats_callback",
             ),
-            InlineKeyboardButton(text="Support 👨", url="t.me/WBBSupport"),
+            InlineKeyboardButton(text="Support 👨", url="t.me/RwSZay"),
         ],
     ]
 )
